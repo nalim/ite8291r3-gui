cf27095c9463742226ae144a92b5da6962ec289c
Milan Votava <votava@i3.cz>
2021-08-29 19:59:32 +0200

    lamdas, persistent state

--- app/ite_tray.py
+++ app/ite_tray.py
@@ -3,16 +3,64 @@ from PyQt5.QtWidgets import *
 from ite8291r3_ctl import ite8291r3
 import webbrowser
 import os
-
+import usb
 
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+home = os.environ['HOME']
+config_file = home + "/.config/ite8291r3-ctl"
+#print(home);
 
-ite = ite8291r3.get()
+#ite = ite8291r3.get()
+#ite_dev = ite8291r3.get()
 
+def store_config(opts):
+    f = open(config_file, "w")
+    f.write(opts)
+    f.close()
+
+try:
+    f = open(config_file)
+    ite_cfg_data=f.read()
+    f.close
+    ite_cfg=ite_cfg_data.rstrip().split(',')
+    del ite_cfg[-1]
+except FileNotFoundError:
+    ite_cfg = ["effect", "rainbow"]
+
+#print(ite_cfg)
+exit
+
+#def brightness(delta):
+#    d = ite()
+#    d.set_brightness(d.get_brightness() + delta)
+
+def effect(h, data):
+    h.set_effect(data(**{"brightness": h.get_brightness()})) 
+
+def cfg():
+    return ite_cfg
+
+def ite():
+    return ite8291r3.get()
+    #return ite_dev
+
+def itel(l, cfg = None, brightness = False):
+    h = ite()
+    l(h)
+    dev = h.channel.dev
+    if cfg:
+        ite_cfg=cfg
+        cfg_data=",".join(cfg)
+        #print(cfg_data)
+        if brightness:
+            b = h.get_brightness()
+            cfg_data = cfg_data + ',' + str(b)
+        store_config(cfg_data)
+    dev._ctx.dispose(dev)
 
 app = QApplication([])
 app.setQuitOnLastWindowClosed(False)
@@ -36,7 +84,8 @@ for k,v in ite8291r3.effects.items():
     action = QAction(k.capitalize())
     qactions_effects[k] = action
     effect_menu.addAction(action)
-    exec("action.triggered.connect(lambda: ite.set_effect( ite8291r3.effects[\""+k+"\"]()) )")
+    #action.triggered.connect(lambda checked, key=k, value=v : itel(lambda h: h.set_effect(value()), ["effect", key], True))
+    action.triggered.connect(lambda checked, key=k, value=v : itel(lambda h: effect(h, value), ["effect", key], True))
 
 menu.addMenu(effect_menu)
 
@@ -58,7 +107,7 @@ for k,v in colors.items():
     action = QAction(k.capitalize())
     qactions_colors[k] = action
     color_menu.addAction(action)
-    exec("action.triggered.connect(lambda: ite.set_color("+str(v)+"))")
+    action.triggered.connect(lambda checked, key=k, value=v : itel(lambda h : h.set_color(value), ["monocolor", key], True))
 
 menu.addMenu(color_menu)
 
@@ -69,33 +118,33 @@ menu.addSeparator() #===========================================================
 
 incbrightness = QAction("Increase Brightness")
 menu.addAction(incbrightness)
-exec("incbrightness.triggered.connect(lambda: ite.set_brightness(ite.get_brightness()+10) )")
+incbrightness.triggered.connect(lambda: itel(lambda h : h.set_brightness(min(h.get_brightness() + 10, 50)), cfg(), True))
 
 decbrightness = QAction("Decrease Brightness")
 menu.addAction(decbrightness)
-exec("decbrightness.triggered.connect(lambda: ite.set_brightness(ite.get_brightness()-10) )")
+decbrightness.triggered.connect(lambda : itel(lambda h : h.set_brightness(max(h.get_brightness() - 10, 0)), cfg(), True))
 
 menu.addSeparator() #================================================================
 
 
 turnoff = QAction("Turn Off Keyboard Backlight")
 menu.addAction(turnoff)
-exec("turnoff.triggered.connect(lambda: ite.turn_off() )")
+turnoff.triggered.connect(lambda : itel(lambda h : h.turn_off(), ["off"]))
 
-freeze = QAction("Freeze Animation")
-menu.addAction(freeze)
-exec("freeze.triggered.connect(lambda: ite.freeze() )")
+#freeze = QAction("Freeze Animation")
+#menu.addAction(freeze)
+#freeze.triggered.connect(lambda: itel(lambda h : h.freeze()))
 
 testpattern = QAction("Test Pattern")
 menu.addAction(testpattern)
-exec("testpattern.triggered.connect(lambda: ite.test_pattern() )")
+testpattern.triggered.connect(lambda : itel(lambda h : h.test_pattern()))
 
 menu.addSeparator() #================================================================
 
 
 about = QAction("About")
 project_webpage_url = "https://github.com/salihmarangoz/ite8291r3-gui"
-exec("about.triggered.connect(lambda: webbrowser.open(project_webpage_url) )")
+about.triggered.connect(lambda : webbrowser.open(project_webpage_url) )
 menu.addAction(about)
 
 
